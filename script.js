280c1ab7727087876ffca03e5cecbdb74fbd6ea6
jsandspxx34-sys <jsandspxx34@gmail.com>
2025-12-12 17:14:36 +0000

    Particles follow mouse

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+let mouse = { x: null, y: null };
+
+window.addEventListener("mousemove", (e) => {
+    mouse.x = e.x;
+    mouse.y = e.y;
+});
+
 // Purple Lightning Floating Neon Particles
 console.log("Purple Lightning Website Loaded!");
 
@@ -20,28 +27,21 @@ canvas.height = window.innerHeight;
 // Particle array
 let particles = [];
 
-class Particle {
-    constructor() {
-        this.x = Math.random() * canvas.width;
-        this.y = Math.random() * canvas.height;
-        this.size = Math.random() * 4 + 1;
-        this.speedX = (Math.random() - 0.5) * 1.5;
-        this.speedY = (Math.random() - 0.5) * 1.5;
-        this.color = `hsl(${Math.random() * 360}, 100%, 70%)`;
-    }
-    update() {
-        this.x += this.speedX;
-        this.y += this.speedY;
-        if(this.x < 0 || this.x > canvas.width) this.speedX *= -1;
-        if(this.y < 0 || this.y > canvas.height) this.speedY *= -1;
-    }
-    draw() {
-        ctx.fillStyle = this.color;
-        ctx.shadowColor = this.color;
-        ctx.shadowBlur = 15;
-        ctx.beginPath();
-        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
-        ctx.fill();
+update() {
+    // Move randomly
+    this.x += this.speedX;
+    this.y += this.speedY;
+
+    // Bounce off edges
+    if(this.x < 0 || this.x > canvas.width) this.speedX *= -1;
+    if(this.y < 0 || this.y > canvas.height) this.speedY *= -1;
+
+    // Move slightly toward mouse
+    if(mouse.x && mouse.y){
+        let dx = mouse.x - this.x;
+        let dy = mouse.y - this.y;
+        this.x += dx * 0.002; // 0.002 = speed toward mouse
+        this.y += dy * 0.002;
     }
 }
 
